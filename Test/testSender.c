69f7c02c5ab42a71cdf8e2f6d188599a763084f8
Yash Parsai (MS/ECF3-XC) <yash.parsai@in.bosch.com>
2022-09-08 13:52:02 +0530

    adding test file for testing cource

--- Test/testSender.c
+++ Test/testSender.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#include "BMS_Sender.h"
+
+int dummyTemp[5] = {5,6,7,8,9};
+int dummySOC[5] = {20,21,22,23,24};
+
+int main()
+{
+	test_getSensorData();
+}
+
+void test_getSensorData(void)
+{
+	const char* sensor_path = "testSensor.csv";
+	struct BMSData BMS_Data[50];
+
+	readingCount = getSensorData(sensor1_path, BMS_Data);
+
+	for(i=0;i<5;i++ )
+	{
+		if(!((BMS_Data[i].temp == dummyTemp[i]) &&
+		     (BMS_Data[i].soc  == dummySOC[i])  ))
+		{
+			assert(0);
+		}
+	}
+}

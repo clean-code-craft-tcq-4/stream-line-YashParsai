42ae9b89d6f315d99b67dca1676cb8d09d304765
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-09-12 12:06:42 +0530

    Update testSender.c

--- Sender/testSender.c
+++ Sender/testSender.c
@@ -4,17 +4,14 @@
 
 void test_getSensorData(void)
 {
-	const char* testsensor_path = "testSensor.csv";
 	struct BMSData BMS_Data[50];
-	int readingCount;
-	int i;
 
 	getRandomSensorData(BMS_Data);
 
-	assert(BMS_Data.temp[10] <= TEMP_MAX);
-	assert(BMS_Data.temp[5]  >= TEMP_MIN);
-	assert(BMS_Data.soc[45]  <= SOC_MAX);
-	assert(BMS_Data.soc[1]  >= SOC_MIN);
+	assert(BMS_Data[10].temp <= TEMP_MAX);
+	assert(BMS_Data[5].temp  >= TEMP_MIN);
+	assert(BMS_Data[45].soc  <= SOC_MAX);
+	assert(BMS_Data[1].soc   >= SOC_MIN);
 }
 
 int main()

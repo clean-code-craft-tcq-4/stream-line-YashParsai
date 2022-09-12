4c208e154c1cbffafeb615dc6e7974e362181078
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-09-12 11:59:53 +0530

    Update BMS_Sender.h

--- Sender/BMS_Sender.h
+++ Sender/BMS_Sender.h
@@ -4,14 +4,7 @@
 #define SOC_MAX 80
 #define TEMP_MIN 5
 #define TEMP_MAX 45
-/*
-#define SENSOR1 1
-#define SENSOR2 2
 
-#define SENSOR_FILE1 "sensor1.csv"
-#define SENSOR_FILE2 "sensor2.csv"
-#define SENSOR_PATH(sensor_id) SENSOR_FILE ## sensor_id 
-*/
 #define MAX_STREAMING_RANGE 50
 
 typedef struct BMSData
@@ -21,8 +14,6 @@ typedef struct BMSData
 }BMSData;
 
 
-int getSensorData(const char* sensor_path, BMSData* BMS_Data);
+void getRandomSensorData(BMSData* BMS_Data);
 void PrintBMSData(int sensor_id, BMSData* BMS_Data, int readings);
 void PrintToConsole(char* buffer);
-
-

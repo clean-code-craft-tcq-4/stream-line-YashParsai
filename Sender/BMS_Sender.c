fb27c9117ba236dd26347ec040853646e275eeda
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-09-12 12:08:17 +0530

    Update BMS_Sender.c

--- Sender/BMS_Sender.c
+++ Sender/BMS_Sender.c
@@ -9,8 +9,8 @@ void getRandomSensorData(BMSData* BMS_Data)
 {
 	for(int index = 0; index < MAX_STREAMING_RANGE ; index++)
 	{
-		BMS_Data->temp[index] = (rand()%(TEMP_MAX - TEMP_MIN + 1)) + TEMP_MIN;
-		BMS_Data->soc[index] = (rand()%(SOC_MAX - SOC_MIN + 1)) + SOC_MIN;
+		BMS_Data[index]->temp = (rand()%(TEMP_MAX - TEMP_MIN + 1)) + TEMP_MIN;
+		BMS_Data[index]->soc = (rand()%(SOC_MAX - SOC_MIN + 1)) + SOC_MIN;
 	}
 }
 void PrintBMSData(int sensor_id, BMSData* BMS_Data, int readings)

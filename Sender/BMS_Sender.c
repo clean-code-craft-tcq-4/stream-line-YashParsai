3dd09b9b9c0572e352e8992eedee5cde4c522bf8
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-09-12 11:59:18 +0530

    Update BMS_Sender.c

--- Sender/BMS_Sender.c
+++ Sender/BMS_Sender.c
@@ -5,40 +5,21 @@
 
 #include "BMS_Sender.h"
 
-int getSensorData(const char* sensor_path, BMSData* BMS_Data)
+void getRandomSensorData(BMSData* BMS_Data)
 {
-	char row[12];
-	char* value;
-	int readings;
-
-	FILE* fp = fopen(sensor_path, "r");
-
-	if (!fp)
+	for(int index = 0; index < MAX_STREAMING_RANGE ; index++)
 	{
-		printf("Unable to open file\n");
-		assert(0);
+		BMS_Data->temp[index] = (rand()%(TEMP_MAX - TEMP_MIN + 1)) + TEMP_MIN;
+		BMS_Data->soc[index] = (rand()%(SOC_MAX - SOC_MIN + 1)) + SOC_MIN;
 	}
-
-	for(readings = 0; (fgets(row,12, fp)) && (readings < MAX_STREAMING_RANGE); readings++)
-	{
-			value = strtok(row, ",");
-			BMS_Data[readings].temp = atoi(value);
-			value = strtok(NULL, ", ");
-			BMS_Data[readings].soc = atoi(value);
-	}
-
-	fclose(fp);
-	return readings;
-
 }
-
 void PrintBMSData(int sensor_id, BMSData* BMS_Data, int readings)
 {
 	char buffer[350] = {0};
 	char* next_addr = buffer;
 	int count;
 
-	for(count = 0; count < readings; count++)
+	for(count = 0; count < readings ; count++)
 	{
 		sprintf(next_addr,"%d,%d,%d\n", sensor_id, BMS_Data[count].temp, BMS_Data[count].soc);
 		next_addr = strchr(buffer,0);
@@ -51,15 +32,3 @@ void PrintToConsole(char* buffer)
 {
 	printf("%s",buffer);
 }
-
-/*int main()
-{
-	struct BMSData BMS_Data[50];
-	int readingCount;
-	int sensor_id=1;
-	const char* sensor1_path = "sensor1.csv";
-
-	readingCount = getSensorData(sensor1_path, BMS_Data);
-	PrintBMSData(sensor_id, BMS_Data, readingCount);
-}
-*/
